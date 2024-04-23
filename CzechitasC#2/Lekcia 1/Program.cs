90e6cd377d20de29c5783874976f336843e1ebab
nikolasekelova <nikola.sekelova@thermofisher.com>
2024-04-23 19:57:11 +0200

    Add project files.

---
+++ CzechitasC#2/Lekcia 1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Lekcia_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lekcia C#2");
+            Rytier albert = new Rytier("Albert", 3);
+            albert.VypisJmenoAZivoty();
+            albert.datumNarozeni = DateTime.Now; // okamzity cas v PC
+            albert.datumNarozeni = new DateTime(1993, 11, 11);
+          //  albert.datumNarozeni = albert.datumNarozeni.AddHours(12).AddDays(3);
+            Console.WriteLine($"Narodil som sa { albert.datumNarozeni.Date}.{albert.datumNarozeni.Month}.{albert.datumNarozeni.Year}.");
+
+            TimeSpan vek = DateTime.Now - albert.datumNarozeni;
+            DateTime novyCas = new DateTime(1993, 11, 11, 18, 20, 0);
+            TimeSpan odpocetHodinMinut = novyCas - albert.datumNarozeni;
+
+            Console.WriteLine($"Moj vek je {albert.datumNarozeni}.");
+
+            Console.WriteLine($"Moj vek je {odpocetHodinMinut.TotalDays / 365}.");
+            Console.WriteLine($"Moj vek je {vek.Days/365}.");
+        }
+
+       
+    }
+}
---
+++ CzechitasC#2/Lekcia 1/Rytier.cs
@@ -0,0 +1,20 @@
+﻿namespace Lekcia_1
+{
+    internal class Rytier
+    {
+        public string Jmeno { get; set; }
+        public int PocetZivotu { get; set; }
+        public DateTime datumNarozeni;
+
+        public Rytier(string jmeno, int pocetZivotu)
+        {
+            Jmeno = jmeno;
+            PocetZivotu = pocetZivotu;
+        }
+
+        public void VypisJmenoAZivoty()
+        {
+            Console.WriteLine($"Jmenuji se {Jmeno} a mam pocet zivotu {PocetZivotu}");
+        }
+    }
+}
